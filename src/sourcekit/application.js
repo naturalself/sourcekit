17dcb4a6d0072804957d893597dc5624049cd9e5
Ken Chau <kchau@g2llc.com>
2011-01-20 23:45:15 -0800

    yar, closure lib sir

--- src/sourcekit/application.js
+++ src/sourcekit/application.js
@@ -1,11 +1,23 @@
-define(['ace'], 'sourcekit/application', function (ace) {
-    return { 
-        start: function() {
-            dojo.require("dijit.layout.ContentPane");
-            dojo.require("dijit.layout.BorderContainer");
-            dojo.require("dijit.layout.TabContainer");
-           // ace.edit("aceWorthy");
-        }
-    }
-});
+goog.provide("sourcekit.Application");
 
+goog.require("goog.dom");
+goog.require("goog.ui.SplitPane");
+goog.require("goog.ui.tree.TreeControl");
+goog.require("goog.ui.TabBar");
+goog.require("goog.ui.Tab");
+
+sourcekit.Application = function() {
+    
+}
+
+sourcekit.Application.run = function() {
+    var tabBar = new goog.ui.TabBar();
+    tabBar.decorate(goog.dom.getElement("top"));
+
+   /* var treeConfig = goog.ui.tree.TreeControl.defaultConfig;
+    tree = new goog.ui.tree.TreeControl('root', treeConfig);
+    
+    tree.render(goog.dom.getElement('treeContainer'));*/
+//    var sp = new goog.ui.SplitPane(tree, tabBar);
+ 
+}
