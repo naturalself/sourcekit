9a7e4fa10b11e4af88091ee6e535a93dc639e7b8
Ken Chau <ken@gizzar.com>
2011-02-06 14:43:14 -0800

    can load and edit files now!!

--- src/sourcekit/editor.js
+++ src/sourcekit/editor.js
@@ -1,42 +1,82 @@
 define('sourcekit/editor', 
         ['sourcekit/fileutil', 
-        'ace/editor', 
+        'ace/editor',
+        'ace/edit_session', 
         "ace/virtual_renderer", 
-        "ace/theme/textmate"], 
+        "ace/theme/twilight"], 
         function(FileUtil) {
 
 dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.layout.TabContainer");
 
 var AceEditor = require("ace/editor").Editor;
+var AceEditSession = require("ace/edit_session").EditSession;
 var Renderer = require("ace/virtual_renderer").VirtualRenderer;
-var theme = require("ace/theme/textmate");
+var theme = require("ace/theme/twilight");
 
 var Editor = function(dropbox, editorEnv) {
     this.dropbox = dropbox;
+    this.sessions = {};
     dojo.addOnLoad(this.setupInterface.bind(this));
 };
 
 Editor.prototype.setupInterface = function() {
     this.tabContainer = dijit.byId("editorTabContainer");
+
+    var theme = require("ace/theme/twilight");
+    this.editorContainer = dojo.byId("editorContainer");
+    this.editor = new AceEditor(new Renderer(this.editorContainer, theme));
+
+    dojo.connect(this.tabContainer, "selectChild", this.onSelectTab.bind(this));
+    dojo.connect(window, "onresize", this.onResize.bind(this));
 }
 
 Editor.prototype.openFile = function(item) {
-    console.log("GOT IT!", item.path);
+    var id = FileUtil.uniqueIdByPath(item.path);
     
-    var editorContentPane = new dijit.layout.ContentPane({
-        title: FileUtil.basename(item.path),
-        content: dojo.create("div", {id: 'editor', style: 'width: 100%; height: 100%'}),
-        closable: true
-    });
+    if (!this.sessions[id]) {
+        // Load the content
+        this.dropbox.getFileContents(item.path, (function(data) {
+            this.sessions[id] = new AceEditSession(data);
+            this.editor.setSession(this.sessions[id]);
+        }).bind(this));
 
-    this.tabContainer.addChild(editorContentPane);
-    var theme = require("ace/theme/textmate");
-    var container = document.getElementById("editor");
-    var editor = new AceEditor(new Renderer(container, theme));
+        // Deal with the UI
+        var editorContentPane = new dijit.layout.ContentPane({
+            title: FileUtil.basename(item.path),
+            content: "",
+            closable: true,
+            id: id
+        });
 
-    dojo.connect(window, "onResize", editor.resize.bind(editor));
-    editor.resize();
+        this.tabContainer.addChild(editorContentPane);
+        this.tabContainer.selectChild(editorContentPane, true);
+    } else {
+        this.tabContainer.selectChild(id, true);
+    }
+}
+
+Editor.prototype.onResize = function(event) {
+    contentBox = dojo.contentBox(this.editorContainer.parentNode);
+    dojo.contentBox(this.editorContainer, contentBox);
+    this.editor.resize();
+}
+
+Editor.prototype.onSelectTab = function(tab) {
+    this.editorContainer.style.display = "block";
+    this.editorContainer.focus();    
+    
+    if (typeof tab == 'string') {
+        tab = dijit.byId(tab);
+    }
+    
+    if (this.sessions[tab.id]) {
+        this.editor.setSession(this.sessions[tab.id]);
+    }
+    
+    dojo.connect(tab, "resize", this.onResize.bind(this));
+    tab.domNode.appendChild(this.editorContainer);
+    this.onResize();
 }
 
 return Editor;
--- src/sourcekit/fileutil.js
+++ src/sourcekit/fileutil.js
@@ -3,6 +3,11 @@ define("sourcekit/fileutil", function() {
 var FileUtil = {
     basename: function(path) {
         return path.replace(/\\/g,'/').replace( /.*\//, '' );
+    },
+    
+    uniqueIdByPath: function(path) {
+        // TODO: BUG!! Use a hashing algorithm for this!
+        return path.replace(/[ \/\.]/g, '_');
     }
     
 }
