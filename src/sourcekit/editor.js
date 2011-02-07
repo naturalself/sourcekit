9d01b85cdca7f30702af553629bab4278a37527c
Ken Chau <ken@gizzar.com>
2011-02-06 21:30:00 -0800

    adding capabilities to close tabs and adding undo

--- src/sourcekit/editor.js
+++ src/sourcekit/editor.js
@@ -1,18 +1,23 @@
 define('sourcekit/editor', 
         ['sourcekit/fileutil', 
         'ace/editor',
-        'ace/edit_session', 
+        'ace/edit_session',
+        'ace/undomanager',
         "ace/virtual_renderer", 
         "ace/theme/twilight"], 
         function(FileUtil) {
 
 dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.layout.TabContainer");
+dojo.require("dijit.Toolbar");
 
 var AceEditor = require("ace/editor").Editor;
 var AceEditSession = require("ace/edit_session").EditSession;
+var AceUndoManager = require("ace/undomanager").UndoManager;
+
 var Renderer = require("ace/virtual_renderer").VirtualRenderer;
 var theme = require("ace/theme/twilight");
+var blankSession = new AceEditSession("");
 
 var Editor = function(dropbox, editorEnv) {
     this.dropbox = dropbox;
@@ -28,6 +33,7 @@ Editor.prototype.setupInterface = function() {
     this.editor = new AceEditor(new Renderer(this.editorContainer, theme));
 
     dojo.connect(this.tabContainer, "selectChild", this.onSelectTab.bind(this));
+    dojo.connect(this.tabContainer, "removeChild", this.onCloseTab.bind(this));
     dojo.connect(window, "onresize", this.onResize.bind(this));
 }
 
@@ -35,9 +41,12 @@ Editor.prototype.openFile = function(item) {
     var id = FileUtil.uniqueIdByPath(item.path);
     
     if (!this.sessions[id]) {
+        this.editor.setSession(blankSession);
+        
         // Load the content
         this.dropbox.getFileContents(item.path, (function(data) {
             this.sessions[id] = new AceEditSession(data);
+            this.sessions[id].setUndoManager(new AceUndoManager());
             this.editor.setSession(this.sessions[id]);
         }).bind(this));
 
@@ -48,6 +57,18 @@ Editor.prototype.openFile = function(item) {
             closable: true,
             id: id
         });
+        
+        var editorToolbar = new dijit.Toolbar({
+            id: id + "_toolbar"
+        });
+        
+        saveButton = new dijit.form.Button({
+            label: "Save",
+            showLabel: true,
+            iconClass: "dijitIconFile"
+        });
+        editorToolbar.addChild(saveButton);
+        editorContentPane.domNode.appendChild(editorToolbar.domNode);
 
         this.tabContainer.addChild(editorContentPane);
         this.tabContainer.selectChild(editorContentPane, true);
@@ -58,13 +79,17 @@ Editor.prototype.openFile = function(item) {
 
 Editor.prototype.onResize = function(event) {
     contentBox = dojo.contentBox(this.editorContainer.parentNode);
-    dojo.contentBox(this.editorContainer, contentBox);
-    this.editor.resize();
+    
+    if (dojo.byId(this.editorContainer.parentNode.id + "_toolbar")) {
+        toolbarMarginBox = dojo.marginBox(dojo.byId(this.editorContainer.parentNode.id + "_toolbar"));
+        contentBox.h = contentBox.h - toolbarMarginBox.h;
+        dojo.contentBox(this.editorContainer, contentBox);
+        this.editor.resize();
+    }
 }
 
 Editor.prototype.onSelectTab = function(tab) {
     this.editorContainer.style.display = "block";
-    this.editorContainer.focus();    
     
     if (typeof tab == 'string') {
         tab = dijit.byId(tab);
@@ -76,7 +101,27 @@ Editor.prototype.onSelectTab = function(tab) {
     
     dojo.connect(tab, "resize", this.onResize.bind(this));
     tab.domNode.appendChild(this.editorContainer);
-    this.onResize();
+    this.tabContainer.resize();
+    this.editor.focus();
+}
+
+Editor.prototype.onCloseTab = function(tab) {
+    
+    
+    if (typeof tab == 'string') {
+        tab = dijit.byId(tab);
+    }
+    
+    if (this.sessions[tab.id]) {
+        delete this.sessions[tab.id];
+    }
+    
+    if (this.tabContainer.getChildren().length > 0) {
+        var index = this.tabContainer.forward();
+    } else {
+        dojo.body().appendChild(this.editorContainer);
+        this.editorContainer.style.display = "none";
+    }
 }
 
 return Editor;
