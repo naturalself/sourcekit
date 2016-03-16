0212a2febd6d7ae15c9707823557656d098864a3
Ken Chau <ken@gizzar.com>
2016-03-15 22:01:41 -0700

    get rid of the extension storage stuff

--- src/background.js
+++ src/background.js
@@ -1,21 +1,3 @@
-// Javascript code to handle options for SourceKit
-
-var _registeredStorageExtensions = {};
-
-// The message handler to accept the registrations.
-function acceptExtensions(request, sender, sendResponse) {
-  if (request.SourceKitRegistration) {
-    if (!(sender.id in _registeredStorageExtensions)) {
-      _registeredStorageExtensions[sender.id] = request.name;
-    }
-    sendResponse(true);
-  }
-}
-function init() {
-  chrome.extension.onRequestExternal.addListener(acceptExtensions);
-}
-window.addEventListener('load',init);
-
 chrome.browserAction.onClicked.addListener(function(activeTab){
   var newURL = "/resources/application.html";
   chrome.tabs.create({ url: newURL });
